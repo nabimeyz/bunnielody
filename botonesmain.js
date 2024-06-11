8c0218de7a987fd8d81c586fd409f5a75a6ec0ff
nabimeyz <lunitahugr@gmail.com>
2024-06-11 12:43:45 -0600

    cambia la pagina index

---
+++ botonesmain.js
@@ -0,0 +1,9 @@
+function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "flex") {
+        div.style.display = "none";
+    }
+    else {
+        div.style.display = "flex";
+    }
+}

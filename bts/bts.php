d90193ea163ae27fecfb857b946eee1b0c7892ba
nabimeyz <lunitahugr@gmail.com>
2024-06-14 22:46:44 -0600

    crea archivos php

--- bts/bts.php
+++ bts/bts.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0  maximum-scale=1.0, user-scalable=no">
     <!--Titulo e icono de barra de navegación-->
-    <title>Bunnielody - NewJeans</title>
+    <title>Bunnielody - BTS</title>
     <link rel="icon" href="/imagenes/conejito.png" type="image/x-icon">
 
     <!--enlaces a hojas de estilo-->
-    <link rel="stylesheet" href="/styles/njMain.css">
+    <link rel="stylesheet" href="/styles/btsMain.css">
 
     <!--Importación de fuentes externas: tipografías-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -17,15 +17,15 @@
     <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-    <!--importación de elementos para funcionamiento del carrusel-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <!--Vinculo archivos JS:-->
-    <script src="/carrusel.js" defer></script>
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
     
 
 <!--inicio del body-->
 <body>
+    
     <header> <!--Parte superior de la página-->
 
         <div> <!--Logo-->
@@ -34,11 +34,10 @@
     
         <nav> <!--Barra de navegación-->
             <ul>
-                <a href="/bts/bts.html"><li>BTS</li></a>
-                <a href="/nj/newJeans.html"><li>NewJeans</li></a>
-                <a href="/txt/tubatu.html"><li>TXT</li></a>
+                <a href="/bts/bts.php"><li>BTS</li></a>
+                <a href="/nj/newJeans.php"><li>NewJeans</li></a>
+                <a href="/txt/tubatu.php"><li>TXT</li></a>
                 <a href="/quienesSomos/quienesSomos.html"><li>Quienes Somos</li></a>
             </ul>
         </nav>
     </header>
-
